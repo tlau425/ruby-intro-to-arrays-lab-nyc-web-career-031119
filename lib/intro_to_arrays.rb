727c6582502c24a07828b513316913432cf5fd56
Tommy Lau <tlau425@yahoo.com>
2019-02-23 05:13:51 +0000

    Done.

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,33 @@
+def instantiate_new_array
+  new_array = []
+  return new_array
+end
+
+def array_with_two_elements
+  new_array = ["a","b"]
+  return new_array
+end
+
+def first_element(array)
+  return array.first
+end
+
+def third_element(array)
+  return array[2]
+end
+
+def last_element(array)
+  return array.last
+end
+
+def first_element_with_array_methods(array)
+  return array.first
+end
+
+def last_element_with_array_methods(array)
+  return array.last
+end
+
+def length_of_array(array)
+  return array.length
+end
