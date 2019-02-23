f08a90b7f61256b212336ba02d6d4fe64f8bb021
Tommy Lau <tlau425@yahoo.com>
2019-02-23 05:09:26 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -9,5 +9,12 @@ def array_with_two_elements
 end
 
 def first_element(array)
-  return array[0]
+  return array.first
 end
+
+def third_element(array)
+  return array[2]
+end
+
+def last_element(array)
+  return array.last
